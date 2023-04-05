7aa57016c073e6d8098086d43096d2a3809597f9
Scott Trinh <scott@ca.la>
2023-04-05 13:35:28 -0400

    Move edgedb client to separate module

--- authOptions.ts
+++ authOptions.ts
@@ -1,13 +1,11 @@
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import * as edgedb from "edgedb";
 
 import EdgeDBAdapter from "@/authAdapter";
 import { getServerConfig } from "@/config";
+import { client } from "@/edgedb";
 
 const { GOOGLE_ID, GOOGLE_SECRET } = getServerConfig();
-
-const edgeDBClient = edgedb.createClient();
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -16,5 +14,5 @@ export const authOptions: AuthOptions = {
       clientSecret: GOOGLE_SECRET,
     }),
   ],
-  adapter: EdgeDBAdapter(edgeDBClient)
+  adapter: EdgeDBAdapter(client)
 }
