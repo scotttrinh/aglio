83707182923d4b24898b3ab936e611ade04dba6b
Scott Trinh <scott@scotttrinh.com>
2023-05-23 11:12:31 -0400

    Get edgedb bump from `main` and no nix packaging

--- flake.nix
+++ flake.nix
@@ -2,45 +2,34 @@
   description = "A Nix-flake-based Node.js development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
-    npmlock2nix = {
-      url = "github:nix-community/npmlock2nix";
-      flake = false;
-    };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , npmlock2nix
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [
-        (final: prev: {
-          npmlock2nix = import npmlock2nix { pkgs = prev; };
-        })
-      ];
-      pkgs = import nixpkgs { inherit overlays system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
       edgedb-dev = pkgs.edgedb.overrideAttrs (oldAttrs: rec {
         src = pkgs.fetchFromGitHub {
           owner = "edgedb";
           repo = "edgedb-cli";
-          rev = "01bb835e92fcdfce73df18d2daf3940249e51cde";
-          sha256 = "sha256-V+tzx9k9uL/E2MzEGLqQa4R7uC7SU7S+2P2zT58AxLQ=";
+          rev = "f4fc73eac35426bc119afa0a1b42c31235c08f53";
+          sha256 = "sha256-YInv9Yh9SYHJ5jvb90lcB58xIymAcUAhddnmkXLA7P0=";
           fetchSubmodules = true;
         };
         cargoDeps = pkgs.rustPlatform.importCargoLock {
           lockFile = src + "/Cargo.lock";
           outputHashes = {
-            "edgedb-derive-0.4.0" = "sha256-soUc9LkDIKEcMzqpFyWJlwcNr4VxF3C1CP6zkibxNU0=";
-            "edgeql-parser-0.1.0" = "sha256-OH078Cp+snMmA30vwsRnEaqx8OzMrQVZllSTZ2GPC8g=";
+            "edgedb-derive-0.5.0" = "sha256-4sk8nAPgiHaC7dXgng4QPMx0oc0voqMj4a1xNiBrppw=";
+            "edgeql-parser-0.1.0" = "sha256-Y3gXxPuR7qnTL4fu2nZIa3e20YV1fLvm2jHAng+Ke2Q=";
             "rexpect-0.5.0" = "sha256-vstAL/fJWWx7WbmRxNItKpzvgGF3SvJDs5isq9ym/OA=";
             "rustyline-8.0.0" = "sha256-CrICwQbHPzS4QdVIEHxt2euX+g+0pFYe84NfMp1daEc=";
             "serde_str-1.0.0" = "sha256-CMBh5lxdQb2085y0jc/DrV6B8iiXvVO2aoZH/lFFjak=";
+            "indexmap-2.0.0-pre" = "sha256-QMOmoUHE1F/sp+NeDpgRGqqacWLHWG02YgZc5vAdXZY=";
           };
         };
         # Patch the Cargo.toml to set the build flag to false
@@ -49,7 +38,7 @@
         '';
       });
       buildInputs = with pkgs; [
-        (nodejs-18_x.override { enableNpm = false; })
+        (nodejs_20.override {enableNpm = false;})
         nodePackages.npm
       ];
       devInputs = with pkgs; [
@@ -58,15 +47,14 @@
         packer
         terraform
       ];
-    in
-    {
+    in {
       devShell = pkgs.mkShell {
         packages = buildInputs ++ devInputs;
 
         shellHook = ''
-          echo "node `${pkgs.nodejs-18_x}/bin/node --version`"
           echo "`${edgedb-dev}/bin/edgedb --version`"
-          echo "`${edgedb-dev}/bin/edgedb instance list`"
+          echo "npm `${pkgs.nodePackages.npm}/bin/npm --version`"
+          echo "node `${pkgs.nodejs_20}/bin/node --version`"
         '';
       };
 
@@ -88,28 +76,29 @@
           '';
         };
 
-        aglio-container = with pkgs.dockerTools; buildImage {
-          name = "aglio";
-          tag = "latest";
-          created = "now";
+        aglio-container = with pkgs.dockerTools;
+          buildImage {
+            name = "aglio";
+            tag = "latest";
+            created = "now";
 
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
-            paths = [
-              aglio
-              binSh
-              pkgs.nodejs-18_x
-            ];
-            pathsToLink = [ "/bin" ];
-          };
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [
+                aglio
+                binSh
+                pkgs.nodejs-18_x
+              ];
+              pathsToLink = ["/bin"];
+            };
 
-          config = {
-            Cmd = [ "sh" "-c" "node /app/.next/server/server.js" ];
-            ExposedPorts = {
-              "3000/tcp" = {};
+            config = {
+              Cmd = ["sh" "-c" "node /app/.next/server/server.js"];
+              ExposedPorts = {
+                "3000/tcp" = {};
+              };
             };
           };
-        };
       };
     });
 }
