452bf126724a72b38c0ad6938ae0fa0536846b5d
Scott Trinh <scott@scotttrinh.com>
2023-05-24 15:23:01 -0400

    Use ts-pattern in playlist API route

--- app/api/playlist/route.ts
+++ app/api/playlist/route.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { NextRequest, NextResponse } from "next/server";
+import { match, P } from "ts-pattern";
 
 import e from "@/dbschema/edgeql-js";
 import { getSession } from "@/getSession";
@@ -25,52 +26,63 @@ type PostBody = z.infer<typeof PostBody>;
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const session = await getSession();
 
-  if (session.state === "LOGGED_OUT")
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  return match(session)
+    .with({ state: "LOGGED_OUT" }, () =>
+      NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+    )
+    .with({ state: "LOGGED_IN", client: P.select() }, async (client) => {
+      const json = await request.json();
+      const bodyResult = PostBody.safeParse(json);
 
-  const { client } = session;
-
-  const json = await request.json();
-  const bodyResult = PostBody.safeParse(json);
-
-  if (!bodyResult.success) {
-    return NextResponse.json(bodyResult.error, { status: 400 });
-  }
-
-  const { data: body } = bodyResult;
-
-  if (body.provider !== "youtube" || body.mediaType !== "playlist") {
-    return NextResponse.json({ message: "Not implemented" }, { status: 400 });
-  }
-
-  const {
-    title,
-    thumbnail,
-    providerMeta: provider_meta,
-  } = await getYouTubePlaylist(body.url);
-
-  const result = await e
-    .update(e.User, () => ({
-      set: {
-        sources: {
-          "+=": e
-            .insert(e.Source, {
-              url: body.url,
-              media_type: body.mediaType,
-              provider: body.provider,
-
+      return match(bodyResult)
+        .with({ success: false, error: P.select() }, (error) =>
+          NextResponse.json(error, { status: 400 })
+        )
+        .with(
+          {
+            success: true,
+            data: P.select({ provider: "youtube", mediaType: "playlist" }),
+          },
+          async (body) => {
+            const {
               title,
               thumbnail,
-              provider_meta,
-            })
-            .unlessConflict((source) => ({
-              on: source.url,
-              else: source,
-            })),
-        },
-      },
-    }))
-    .run(client);
+              providerMeta: provider_meta,
+            } = await getYouTubePlaylist(body.url);
 
-  return NextResponse.json(result, { status: 201 });
+            const result = await e
+              .update(e.User, () => ({
+                set: {
+                  sources: {
+                    "+=": e
+                      .insert(e.Source, {
+                        url: body.url,
+                        media_type: body.mediaType,
+                        provider: body.provider,
+
+                        title,
+                        thumbnail,
+                        provider_meta,
+                      })
+                      .unlessConflict((source) => ({
+                        on: source.url,
+                        else: source,
+                      })),
+                  },
+                },
+              }))
+              .run(client);
+
+            return NextResponse.json(result, { status: 201 });
+          }
+        )
+        .with({ success: true }, async (body) => {
+          return NextResponse.json(
+            { message: "Not implemented" },
+            { status: 400 }
+          );
+        })
+        .exhaustive();
+    })
+    .exhaustive();
 }
