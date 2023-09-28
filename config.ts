bb27d5e87c4fb116538efc033fba29be9e58be6b
Scott Trinh <scott@scotttrinh.com>
2023-09-27 22:43:24 -0400

    Add config value for base URL

--- config.ts
+++ config.ts
@@ -3,10 +3,12 @@ import { z } from "zod";
 export const getServerConfig = () =>
   z
     .object({
+      APP_BASE_URL: z.string(),
       EDGEDB_AUTH_BASE_URL: z.string(),
       YOUTUBE_API_KEY: z.string(),
     })
     .parse({
+      APP_BASE_URL: process.env.APP_BASE_URL,
       EDGEDB_AUTH_BASE_URL: process.env.EDGEDB_AUTH_BASE_URL,
       YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
     });
